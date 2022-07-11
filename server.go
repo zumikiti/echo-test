c85048a69a2e6448ee780a5e64172242829ee2ac
akihiro higashiizumi <akihiro@akihironoMac-mini.local>
2022-07-12 08:37:59 +0900

    [add] routes の設定

--- server.go
+++ server.go
@@ -8,8 +8,24 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+
+	// routes
+	e.GET("/users", getUsers)
+	// e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	// e.PUT("/users/:id", updateUser)
+	// e.DELETE("/users/:id", deleteUser)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUsers(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+
+	return c.String(http.StatusOK, id)
 }
