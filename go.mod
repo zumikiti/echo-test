683c7af17a7f361278c2e55a8738101a316fa42a
akihiro higashiizumi <akihiro@akihironoMac-mini.local>
2022-07-14 09:55:19 +0900

    [add] users の取得と登録を実装

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.18
 require github.com/labstack/echo v3.3.10+incompatible
 
 require (
+	github.com/gocraft/dbr v0.0.0-20190714181702-8114670a83bd // indirect
 	github.com/labstack/echo/v4 v4.7.2 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/lib/pq v1.10.6 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
--- server.go
+++ server.go
@@ -3,13 +3,35 @@ package main
 import (
 	"net/http"
 
+	"github.com/gocraft/dbr"
 	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 )
 
-type User struct {
-	Name  string `json:"name" xml:"name" form:"name" query:"name"`
-	Email string `json:"email" xml:"email" form:"email" query:"email"`
-}
+type (
+	UserJson struct {
+		Name  string `json:"name"`
+		Email string `json:"email`
+	}
+
+	User struct {
+		Id       int    `db:id`
+		Name     string `db:name`
+		Email    string `db:email`
+		Password string `db:password`
+	}
+
+	response struct {
+		Users []User
+	}
+)
+
+var (
+	tableName = "users"
+	seq       = 1
+	conn, _   = dbr.Open("postgres", "host=pgsql port=5432 user=sail password=password dbname=db sslmode=disable", nil)
+	sess      = conn.NewSession(nil)
+)
 
 func main() {
 	e := echo.New()
@@ -25,14 +47,23 @@ func main() {
 }
 
 func getUsers(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	var u []User
+	sess.Select("*").From(tableName).Load(&u)
+
+	res := new(response)
+	res.Users = u
+
+	return c.JSON(http.StatusCreated, res)
 }
 
 func getUser(c echo.Context) error {
 	// User ID from path `users/:id`
 	id := c.Param("id")
 
-	return c.String(http.StatusOK, id)
+	var u User
+	sess.Select("*").From(tableName).Where("id = ?", id).Load(&u)
+
+	return c.JSON(http.StatusOK, u)
 }
 
 func saveUser(c echo.Context) error {
@@ -45,6 +76,12 @@ func saveUser(c echo.Context) error {
 	// Get name and email
 	u.Name = c.FormValue("name")
 	u.Email = c.FormValue("email")
+	u.Password = "password"
 
-	return c.JSON(http.StatusCreated, u)
+	_, err := sess.InsertInto(tableName).Columns("name", "email", "password").Values(u.Name, u.Email, u.Password).Exec()
+	if err != nil {
+		return c.String(http.StatusOK, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, u)
 }
